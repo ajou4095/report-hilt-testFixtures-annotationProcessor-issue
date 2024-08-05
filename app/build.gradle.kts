2bdce62ea6875e903cc7db461c2cecebfc47b295
Ray Jang <ajou4095@gmail.com>
2024-08-06 01:52:21 +0900

    Init Commit

---
+++ app/build.gradle.kts
@@ -0,0 +1,68 @@
+import org.jetbrains.kotlin.gradle.dsl.JvmTarget
+
+plugins {
+    alias(libs.plugins.android.application)
+    alias(libs.plugins.kotlin.android)
+    alias(libs.plugins.kotlin.kapt)
+    alias(libs.plugins.ksp)
+    alias(libs.plugins.hilt)
+}
+
+android {
+    namespace = "com.example.hilt"
+    compileSdk = 34
+
+    defaultConfig {
+        applicationId = "com.example.hilt"
+        minSdk = 24
+        targetSdk = 34
+        versionCode = 1
+        versionName = "1.0"
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+    kotlin {
+        compilerOptions {
+            jvmTarget.set(JvmTarget.JVM_17)
+        }
+    }
+    testFixtures {
+        enable = true
+    }
+}
+
+dependencies {
+
+    implementation(libs.androidx.core.ktx)
+    implementation(libs.androidx.appcompat)
+    implementation(libs.material)
+    testImplementation(libs.junit)
+    androidTestImplementation(libs.androidx.junit)
+    androidTestImplementation(libs.androidx.espresso.core)
+
+    implementation(libs.bundles.kotlin)
+    implementation(libs.hilt.android)
+    ksp(libs.hilt.android.compiler)
+
+    testImplementation(libs.bundles.test)
+    kspTest(libs.hilt.android.compiler)
+
+    testFixturesImplementation(libs.bundles.kotlin)
+    testFixturesImplementation(libs.hilt.android)
+    testFixturesImplementation(libs.bundles.test)
+    testFixturesAnnotationProcessor(libs.hilt.android.compiler)
+}
